c62be0724f464f5115492253061340370b18e01c
AqUpd <47220198+AquUpd@users.noreply.github.com>
2021-11-12 22:25:44 +0300

    10%

---
+++ ru/passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Пароль должен содержать как минимум 6 паролей и повторный пароль должен быть таким же как первый пароль.',
+    'reset' => 'Ваш пароль был изменен!',
+    'sent' => 'Мы отправили вам письмо где можно восстановить пароль!',
+    'token' => 'Токен изменения пароля недействителен.',
+    'user' => "Мы не можем найти пользователя с таким E-Mail адресом.",
+];
